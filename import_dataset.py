c52bdf6fefcb4ccca44b46ac60f951c51a62ed84
NCPROGRAMMER <tarheelprogrammer@gmail.com>
2024-10-06 09:25:35 -0400

    Create import_dataset.py

---
+++ import_dataset.py
@@ -0,0 +1,33 @@
+# Importing necessary libraries
+import pymongo
+import sys
+import csv
+
+# Setting a limit for the field size to 2147483647
+csv.field_size_limit(2147483647)
+
+# Connecting to the local MongoDB database
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["enron"]
+collection = db["enronmails"]
+
+# Importing the reader function from the csv library
+from csv import reader
+
+# Opening the 'emails.csv' file in read mode
+with open('emails.csv', 'r') as read_obj:
+    csv_reader = reader(read_obj)
+    
+    # Iterating through each row of the CSV file
+    for row in csv_reader:
+        # Creating a dictionary to store the email data
+        email_data = {}
+        
+        # Storing the contents of the first column in the 'file' key of the dictionary
+        email_data['file'] = row[0]
+        
+        # Storing the contents of the second column in the 'message' key of the dictionary
+        email_data['message'] = row[1]
+        
+        # Inserting the email data into the enronmails10 collection
+        collection.insert_one(email_data)
